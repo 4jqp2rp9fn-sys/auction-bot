db7a9f89a373da421a5c6028fc98892959bf4b75
4jqp2rp9fn-sys <inuxtha@gmail.com>
2026-04-15 19:46:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ from scripts.python import yahoo_auctions_line_alert
 import json
 import os
 
+SEEN_FILE = "seen.json"
+
+def load_seen():
+    if os.path.exists(SEEN_FILE):
+        with open(SEEN_FILE, "r") as f:
+            return set(json.load(f))
+    return set()
+
+def save_seen(seen):
+    with open(SEEN_FILE, "w") as f:
+        json.dump(list(seen), f)
+
 while True:
     yahoo_auctions_line_alert.main()
     time.sleep(300)
